fe3a5c9b8372deb6c482cb209584667484d783b3
Youssefachrafmoussi <37839611+Youssefachrafmoussi@users.noreply.github.com>
2019-07-23 15:03:13 +0200

    Update CabaiaDetails.js

--- CabaiaDetails.js
+++ CabaiaDetails.js
@@ -8,7 +8,7 @@ var checkout = "";
 while (page[i] != ';' && i < page.length - 1) {        checkout += page[i];        i++;    }
 var checkoutjson = $.parseJSON(checkout);
 checkoutToken = checkoutjson.token;
-if(["Point relais"].indexOf(checkoutjson.shipping_rate.title) !=-1)
+if(["Livraison point relais"].indexOf(checkoutjson.shipping_rate.title) !=-1)
 {
     var iframe = '<iframe src ="https://wscartography.crossdesk.com/cms/back/selectedPPT.aspx?ID_COMMANDE='+checkoutToken+'&ID_CLIENT=4ae89fbd-23b1-479f-b368-68bce6177454"></iframe>';
     var IframeT = '<h2 class="os-step__title" id="main-header" tabindex="-1">Votre point relais : </h2>' ; 
