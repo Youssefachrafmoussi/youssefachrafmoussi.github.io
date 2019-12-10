41044704cb4be35c15e865e92bb732f4e8c48e0a
Youssef A <37839611+Youssefachrafmoussi@users.noreply.github.com>
2019-12-11 00:11:55 +0100

    Update testscript2.js

--- testscript2.js
+++ testscript2.js
@@ -1,23 +1,19 @@
-﻿require(["jquery"], function($) {
 
-    var id_client = $("#CrossIdClient").text();
-    var order_Id = $("#CrossOrderId").text();
-    var city = $("#CrossCity").text();
-    var country = $("#CrossCountry").text();
-    var postcode = $("#CrossPC").text();
-    var Cart = '<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;"><br>Le point relais affiche est celui ou sera effectuee la livraison.<br><br><iframe style="border:1px solid #CCCCCC;" id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Front/picker.aspx?ID_COMMANDE='+order_Id+'&ID_CLIENT='+id_client+'&QUERY='+postcode+'+'+city+'&COUNTRY='+country+'" frameborder="0"></iframe><div><br><br>'
-    //var Cart = '<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;"><br><br><iframe style="border:1px solid #CCCCCC;" id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/cms/back/selectedPPT.aspx?ID_COMMANDE='+order_Id+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><br><br>' ;
-    height = 740;
-    try {
-        if (!(document.getElementById('order_data').offsetWidth < 460)) {
-            height = 430;
-        }
-        }
-        catch(err)
-        {}
-    $( "#CrossMap" ).after(Cart);
-    $("#iFrameMap").height(height); 
+jQuery(document).ready(function () {
+        ShowSelectedPPT();
+});
 
-})
+function ShowSelectedPPT(){
+    var id_client = jQuery("#CrossIdClient").text();
+    var id_commande = jQuery("#CrossOrderId").text();
 
-    
+    if(jQuery("#Hook").text()==='DD'){
+        jQuery("#Hook").after('<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;">Point relais :<br><br><iframe style="border:1px solid #CCCCCC;"id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPT.aspx?ID_COMMANDE='+id_commande+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><div class="clear"></div>');
+    }
+    else if(jQuery("#Hook").text()==='FD'){
+        jQuery("#Hook").after('<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;">Point relais :<br><br><iframe style="border:1px solid #CCCCCC;"id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPT.aspx?ID_COMMANDE='+id_commande+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><div class="clear"></div>');
+    }
+    else if(jQuery("#Hook").text()==='FM'){
+        jQuery("#Hook").after('<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;">Point relais :<br><br><iframe style="border:1px solid #CCCCCC;"id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPT.aspx?ID_COMMANDE='+id_commande+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><div class="clear"></div>');
+    }
+}
