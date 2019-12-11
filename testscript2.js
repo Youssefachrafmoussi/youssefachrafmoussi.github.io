f1841ef3b92f6968650db0521bd956a457812d18
Youssef A <37839611+Youssefachrafmoussi@users.noreply.github.com>
2019-12-11 01:07:43 +0100

    Update testscript2.js

--- testscript2.js
+++ testscript2.js
@@ -1,19 +1,21 @@
+var $wk_jq = $.noConflict();
 
-jQuery(document).ready(function () {
+
+$wk_jq(document).ready(function () {
         ShowSelectedPPT();
 });
 
 function ShowSelectedPPT(){
-    var id_client = jQuery("#CrossIdClient").text();
-    var id_commande = jQuery("#CrossOrderId").text();
+    var id_client = $wk_jq("#CrossIdClient").text();
+    var id_commande = $wk_jq("#CrossOrderId").text();
 
-    if(jQuery("#Hook").text()==='DD'){
-        jQuery("#Hook").after('<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;">Point relais :<br><br><iframe style="border:1px solid #CCCCCC;"id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPT.aspx?ID_COMMANDE='+id_commande+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><div class="clear"></div>');
+    if($wk_jq("#Hook").text()==='DD'){
+        $wk_jq("#Hook").after('<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;">Point relais :<br><br><iframe style="border:1px solid #CCCCCC;"id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPT.aspx?ID_COMMANDE='+id_commande+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><div class="clear"></div>');
     }
-    else if(jQuery("#Hook").text()==='FD'){
-        jQuery("#Hook").after('<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;">Point relais :<br><br><iframe style="border:1px solid #CCCCCC;"id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPT.aspx?ID_COMMANDE='+id_commande+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><div class="clear"></div>');
+    else if($wk_jq("#Hook").text()==='FD'){
+        $wk_jq("#Hook").after('<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;">Point relais :<br><br><iframe style="border:1px solid #CCCCCC;"id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPT.aspx?ID_COMMANDE='+id_commande+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><div class="clear"></div>');
     }
-    else if(jQuery("#Hook").text()==='FM'){
-        jQuery("#Hook").after('<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;">Point relais :<br><br><iframe style="border:1px solid #CCCCCC;"id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPT.aspx?ID_COMMANDE='+id_commande+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><div class="clear"></div>');
+    else if($wk_jq("#Hook").text()==='FM'){
+        $wk_jq("#Hook").after('<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;">Point relais :<br><br><iframe style="border:1px solid #CCCCCC;"id="iFrameMap" width="100%" height="430" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPT.aspx?ID_COMMANDE='+id_commande+'&ID_CLIENT='+id_client+'" frameborder="0"></iframe><div><div class="clear"></div>');
     }
 }
