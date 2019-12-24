0506e0c5b6737e68f7bf6b04171bce1f28a74491
Youssef A <37839611+Youssefachrafmoussi@users.noreply.github.com>
2019-12-24 16:30:15 +0100

    Update CabaiaDetails.js

--- CabaiaDetails.js
+++ CabaiaDetails.js
@@ -1,4 +1,21 @@
-$( document ).ready(function() {
+if (document.URL.indexOf("com/account/orders") != -1 && document.body.innerHTML.search("Livraison point relais")) {
+        var shopId = document.getElementById('in-context-paypal-metadata').getAttribute('data-shop-id');
+
+        var pageurl = document.URL;
+  
+        var orderId = pageurl.substring(pageurl.indexOf('account/orders/')+'account/orders/'.length, pageurl.length);
+
+        var shopurl = pageurl.substring(0, pageurl.indexOf('/account/orders/'));;
+
+        var htmlNode ='<br> <div class="myaccount__return-button text-center"><a href="'+shopurl+'/'+shopId+'/orders/'+orderId+'" class="btn account-button__return"><span>Point relais</span></a></div>';
+
+    [...document.querySelectorAll("span")]
+   .filter(a => a.textContent.includes("Adresse de livraison"))
+   .forEach(a => a.nextElementSibling.insertAdjacentHTML('beforeend',htmlNode))
+
+    }
+else{
+    $( document ).ready(function() {
 
 var page = $('html')[0].outerHTML; 
 var start = "Shopify.checkout = ";
@@ -16,3 +33,6 @@ if(["Livraison point relais"].indexOf(checkoutjson.shipping_rate.title) !=-1)
     $( "div.map.default-background" ).replaceWith( iframe );
 }
 });
+    
+}
+
