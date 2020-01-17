e2849f923ea75c628c2cf4d9a511b0204fdc87ed
Youssef A <37839611+Youssefachrafmoussi@users.noreply.github.com>
2020-01-17 13:05:54 +0100

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -20,7 +20,6 @@ window.addEventListener('load', function () {
 function xlInit() {
     if (document.URL.indexOf("tempetedelouest.fr/mon-compte/voir-commande") != -1) {
             var shippingMethod=document.getElementsByClassName("shipped_via")[0].innerHTML;
-            console.log(document.getElementsByClassName("shipped_via")[0].innerHTML);
             if (shippingMethod.includes(xlPptCarriers))
             {
                         xlDisplaySelectedPPT();
@@ -29,5 +28,8 @@ function xlInit() {
     }
 }
 function xlDisplaySelectedPPT() {
-    console.log("SelectedPPT!");
+    id_client="aa43452a-60bc-49d1-8d86-48b81fb32095";
+    order_Id=document.getElementsByClassName("order-number")[0].innerHTML;
+    var Cart = '<div id="mapDiv" class="order_carrier_content box" style="border-top:0px;"><br><br><iframe style="border:1px solid #CCCCCC;" id="iFrameMap" width="100%" height="300" src="https://wscartography.crossdesk.com/cms/back/selectedPPT.aspx?ID_COMMANDE=' + order_Id + '&ID_CLIENT=' + id_client + '" frameborder="0"></iframe><div><br><br>';
+    document.getElementsByClassName("order_details")[0].insertAdjacentHTML('afterend',Cart);
 }
