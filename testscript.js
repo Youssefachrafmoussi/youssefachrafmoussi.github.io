dd17747a31a889182c7ef5913a649aff4a7f3f5c
Youssef A <37839611+Youssefachrafmoussi@users.noreply.github.com>
2020-01-17 12:29:33 +0100

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -12,15 +12,18 @@
 //  ////////////////////////////////////////////////////////////////////////////////////////////////
 
 var xlUrl = "https://wscartography.crossdesk.com";
-var xlPptCarriers = "";
-
-$(document).ready(function () {
-    if (xlCustomerId.xlCustomerId)
+var xlPptCarriers = "Chronopost – Point relais – 24/48h CD";
+window.addEventListener('load', function () {
         xlInit();
-});
+})
+
 function xlInit() {
     if (document.URL.indexOf("tempetedelouest.fr/mon-compte/voir-commande") != -1) {
-        xlDisplaySelectedPPT();
+            if (xlPptCarriers.includes(document.getElementsByClassName("shipped_via")[0].innerHTML))
+            {
+                        xlDisplaySelectedPPT();
+            }
+        
     }
 }
 function xlDisplayMap() {
