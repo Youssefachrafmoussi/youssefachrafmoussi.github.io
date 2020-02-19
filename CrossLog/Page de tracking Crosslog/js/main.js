3e08b726f3c5f6ba7f4a6dafc878d69c269b85c3
Youssef Achraf PC Crosslog <37839611+Youssefachrafmoussi@users.noreply.github.com>
2020-02-19 15:22:45 +0100

    Update main.js

--- CrossLog/Page de tracking Crosslog/js/main.js
+++ CrossLog/Page de tracking Crosslog/js/main.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     else{
       height = 430;
         }
-    document.getElementById("mapDiv").insertAdjacentHTML('afterend','<div id="mapDiv" class="order_carrier_content box" ><iframe id="iFrameMap" width="100%" height="'+height+'" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPTV2.aspx?ID_COMMANDE=TestToken2&ID_CLIENT=4ae89fbd-23b1-479f-b368-68bce6177454&Debug=1" frameborder="0"></iframe><div><div class="clear"></div>');
+    //document.getElementById("mapDiv").insertAdjacentHTML('afterend','<div id="mapDiv" class="order_carrier_content box" ><iframe id="iFrameMap" width="100%" height="'+height+'" src="https://wscartography.crossdesk.com/CMS/Back/selectedPPTV2.aspx?ID_COMMANDE=TestToken2&ID_CLIENT=4ae89fbd-23b1-479f-b368-68bce6177454&Debug=1" frameborder="0"></iframe><div><div class="clear"></div>');
     
     $('body').on('click', 'fa-desktop', function() {
       alert('yes');
