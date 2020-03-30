ef1fb38f4d53f4d3fd2f684449594d7addff5613
Youssef Achraf PC Crosslog <37839611+Youssefachrafmoussi@users.noreply.github.com>
2020-03-30 14:44:25 +0200

    Update main.js

--- CrossLog/Page de tracking Crosslog/js/main.js
+++ CrossLog/Page de tracking Crosslog/js/main.js
@@ -89,5 +89,5 @@ if(width<700){
 else{
   height = 430;
     }
-document.getElementById("mapDiv").insertAdjacentHTML('afterend','<div id="mapDiv" class="order_carrier_content box" ><iframe id="iFrameMap" width="100%" height="'+height+'" src="https://wscartography.crossdesk.com/CMS/Front/picker.aspx?ID_COMMANDE=TestToken2&ID_CLIENT=8f3d093b-3a54-11e3-acfb-d8d385e2f9ec&QUERY=75002+Paris&COUNTRY=FR" frameborder="0"></iframe><div><div class="clear"></div>');
+document.getElementById("mapDiv").insertAdjacentHTML('afterend','');
 });
