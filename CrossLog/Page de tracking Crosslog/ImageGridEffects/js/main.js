f1786280218a9afd53e5ef3b2e24195090456615
Youssef Achraf PC Crosslog <37839611+Youssefachrafmoussi@users.noreply.github.com>
2020-04-08 13:07:15 +0200

    Update main.js

--- CrossLog/Page de tracking Crosslog/ImageGridEffects/js/main.js
+++ CrossLog/Page de tracking Crosslog/ImageGridEffects/js/main.js
@@ -171,7 +171,7 @@
 		if( this.isAnimating || this.isExpanded ) return;
 		this.isAnimating = true;
 		this.isExpanded = true;
-		$("[id$='ButtonPreview']").detach().prependTo('#MadeBy');
+		$("[id$='ButtonPreview']").detach().after('#MadeBy');
 
 		// item's image
 		var gridImg = item.querySelector('img'),
